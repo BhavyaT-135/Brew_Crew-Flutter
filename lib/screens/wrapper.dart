8dbc5c317e274ae96cedc4674cc024a2c5e974a9
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2021-06-26 18:09:08 +0530

    Added sign_in.dart, modified authenticate.dart and wrapper.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,10 @@
-import 'package:brew_crew/screens/home/home.dart';
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
